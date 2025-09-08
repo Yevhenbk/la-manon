4347491c0f075d94831de4c0e801dc0d6022e81e
Yevhenbk <balagutrak.jenia@gmail.com>
2025-09-08 22:51:28 +0200

    fix: Add aria-label to ContactFormButton for improved accessibility

--- src/components/atoms/ContactFormButton.tsx
+++ src/components/atoms/ContactFormButton.tsx
@@ -17,6 +17,7 @@ const ContactFormButton: React.FC<ContactFormButtonProps> = ({
       className="text-sm md:text-lg bg-primary text-black transition-all
         rounded-full border border-white m-0 py-3 px-3 md:px-5 flex justify-center items-center font-semibold w-[160px] md:w-[220px]
         hover:bg-tertiary hover:cursor-pointer"
+      aria-label={contactFormButtonData.label}
     >
       {contactFormButtonData.label}
     </button>
