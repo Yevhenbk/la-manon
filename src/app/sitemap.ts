287980b034558e78786558088dc287c381254ab3
Yevhenbk <balagutrak.jenia@gmail.com>
2025-09-08 22:42:32 +0200

    feat: Add sitemap generation function for SEO enhancement

---
+++ src/app/sitemap.ts
@@ -0,0 +1,9 @@
+const sitemap = () => [
+  { url: "https://www.lamanonsagasta.es/", lastModified: new Date() },
+  {
+    url: "https://www.lamanonsagasta.es/privacy-policy",
+    lastModified: new Date(),
+  },
+];
+
+export default sitemap;
